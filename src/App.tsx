5ea0ef6c16b68627dac75d15ca0653eacf4518cb
Adrian Silva <silvadrian3@gmail.com>
2022-09-08 22:12:21 +0800

    Implement Redux

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import AddToList from './components/AddToList';
 import "./components/List"
 import List from './components/List';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators, State } from './state';
 
 export interface IState {
   people: {
@@ -16,6 +19,10 @@ export interface IState {
 function App() {
 
 
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney, bankruptMoney } = bindActionCreators(actionCreators, dispatch)
+  const amount = useSelector((state: State) => state.bank)
   const [people, setPeople] = useState<IState["people"]>([{
     name: "Lebron James",
     url: "",
@@ -34,8 +41,15 @@ function App() {
       <h1>People Invited to my Party</h1>
       <List people={people} />
       <AddToList people={people} setPeople={setPeople} />
-
+      <hr />
+      <div>
+        <h1>{amount}</h1>
+        <button onClick={()=>depositMoney(1000)}>Deposit</button>
+        <button onClick={()=>withdrawMoney(500)}>Withdraw</button>
+        <button onClick={()=>bankruptMoney()}>Bankrupt</button>
+      </div>
     </div>
+
   );
 }
 
